3718fe6a30f78c61ae10ea09d36d9bd391feaa96
Marko Skugor <mskugor@revlocal.com>
2024-09-30 17:12:28 -0400

    changing copy text

--- components/EventsModal.jsx
+++ components/EventsModal.jsx
@@ -14,7 +14,7 @@ const EventsModal = () => {
         <div className="modal-box font-augusta bg-stone-900 text-stone-100 min-h-75dvh">
           <h3 className="font-bold font-augustashadow text-4xl">Events</h3>
           <p className="py-4">
-            Nothing to see here...
+            Coming Soon!
           </p>
           <form method="dialog">
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
